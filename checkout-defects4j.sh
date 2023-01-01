d1872f7542ea97d3ca9bb82301e7f3ee69ce6855
Matheus Soares <matheusssilva98@gmail.com>
2023-01-01 20:30:31 -0300

    Adiciona checkout de todas as versões e de todos os projetos

--- checkout-defects4j.sh
+++ checkout-defects4j.sh
@@ -5,15 +5,80 @@ main() {
             versao_inicial=1
             versao_final=26
             ;;
+        "cli")
+            projeto="Cli"
+            versao_inicial=1
+            versao_final=40
+            ;;
+        "closure")
+            projeto="Closure"
+            versao_inicial=1
+            versao_final=176
+            ;;
+        "codec")
+            projeto="Codec"
+            versao_inicial=1
+            versao_final=18
+            ;;
+        "collections")
+            projeto="Collections"
+            versao_inicial=25
+            versao_final=28
+            ;;
+        "compress")
+            projeto="Compress"
+            versao_inicial=1
+            versao_final=47
+            ;;
         "csv")
             projeto="Csv"
             versao_inicial=1
             versao_final=16
             ;;
-        "cli")
-            projeto="Cli"
+        "gson")
+            projeto="Gson"
             versao_inicial=1
-            versao_final=40
+            versao_final=18
+            ;;
+        "jackson-core")
+            projeto="JacksonCore"
+            versao_inicial=1
+            versao_final=26
+            ;;
+        "jackson-databind")
+            projeto="JacksonDatabind"
+            versao_inicial=1
+            versao_final=112
+            ;;
+        "jackson-xml")
+            projeto="JacksonXml"
+            versao_inicial=1
+            versao_final=6
+            ;;
+        "jsoup")
+            projeto="Jsoup"
+            versao_inicial=1
+            versao_final=93
+            ;;
+        "jxPath")
+            projeto="JxPath"
+            versao_inicial=1
+            versao_final=22
+            ;;
+        "lang")
+            projeto="Lang"
+            versao_inicial=1
+            versao_final=65
+            ;;
+        "math")
+            projeto="Math"
+            versao_inicial=1
+            versao_final=106
+            ;;
+        "mockito")
+            projeto="Mockito"
+            versao_inicial=1
+            versao_final=38
             ;;
         "time")
             projeto="Time"
