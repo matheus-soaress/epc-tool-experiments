791428c56ccf3155dfd9e32fc1087a4f4bd5135f
Matheus Soares <matheusssilva98@gmail.com>
2023-01-05 14:12:54 -0300

    Correção classes de teste Time

--- get-project-version-cov.sh
+++ get-project-version-cov.sh
@@ -13,18 +13,25 @@ export dir_fonte_classpath="\/PPgSI\/projects\/"$1"\/"$3"b\/"$4
 main() 
 {
     echo $1"-"$3"b"
+    if
+        [ $1 = "time" ]
+        then
+            classe_junit="junit.textui.TestRunner"
+        else
+            classe_junit="org.junit.runner.JUnitCore"
+    fi
     case $2 in
         "jacoco")
-            get_cobertura_jacoco $dir_fonte_classpath
+            get_cobertura_jacoco $dir_fonte_classpath $classe_junit
             ;;
         "node")
-            get_cobertura_nos $dir_fonte_classpath
+            get_cobertura_nos $dir_fonte_classpath $classe_junit
             ;;
         "edge")
-            get_cobertura_arestas $dir_fonte_classpath
+            get_cobertura_arestas $dir_fonte_classpath $classe_junit
             ;;
         "edge-pair")
-            get_cobertura_pares_arestas $dir_fonte_classpath
+            get_cobertura_pares_arestas $dir_fonte_classpath $classe_junit
             ;;
     esac
 }
@@ -35,7 +42,7 @@ get_cobertura_jacoco()
     java -jar $dir_jacoco instrument $dir_fonte --dest $dir_destino_jacoco
     dir_destino_jacoco_cp="\/PPgSI\/out-jacoco\/"
     classpath_jacoco=$(echo $classpath | sed "s,$1,$dir_destino_jacoco_cp,g")
-    java -cp "$dir_jacoco_agent:$classpath_jacoco" org.junit.runner.JUnitCore $classes_teste
+    java -cp "$dir_jacoco_agent:$classpath_jacoco" $2 $classes_teste
     java -jar $dir_jacoco report ./jacoco.exec --classfiles $dir_fonte --xml $dir_xml_jacoco
     rm ./jacoco.exec
 }
@@ -46,7 +53,7 @@ get_cobertura_nos()
     java -jar $dir_epc_tool instrument -src $dir_fonte -dest $dir_destino_epc_tool
     dir_destino_epc_tool_cp="\/PPgSI\/out-epc-tool\/"
     classpath_epc_tool=$(echo $classpath | sed "s,$1,$dir_destino_epc_tool_cp,g")
-    java -cp "$dir_epc_tool:$classpath_epc_tool" org.junit.runner.JUnitCore $classes_teste
+    java -cp "$dir_epc_tool:$classpath_epc_tool" $2 $classes_teste
     java -jar $dir_epc_tool report -input ./coverage.ser -classes $dir_fonte -xml $dir_xml_epc_tool-node.xml
     rm ./coverage.ser
 }
@@ -57,7 +64,7 @@ get_cobertura_arestas()
     java -jar $dir_epc_tool instrument -src $dir_fonte -dest $dir_destino_epc_tool -edges
     dir_destino_epc_tool_cp="\/PPgSI\/out-epc-tool\/"
     classpath_epc_tool=$(echo $classpath | sed "s,$1,$dir_destino_epc_tool_cp,g")
-    java -cp "$dir_epc_tool:$classpath_epc_tool" org.junit.runner.JUnitCore $classes_teste
+    java -cp "$dir_epc_tool:$classpath_epc_tool" $2 $classes_teste
     java -jar $dir_epc_tool report -input ./coverage.ser -classes $dir_fonte -xml $dir_xml_epc_tool-edge.xml -edges
     rm ./coverage.ser
 }
@@ -68,7 +75,7 @@ get_cobertura_pares_arestas()
     java -jar $dir_epc_tool instrument -src $dir_fonte -dest $dir_destino_epc_tool -edge-pairs
     dir_destino_epc_tool_cp="\/PPgSI\/out-epc-tool\/"
     classpath_epc_tool=$(echo $classpath | sed "s,$1,$dir_destino_epc_tool_cp,g")
-    java -cp "$dir_epc_tool:$classpath_epc_tool" org.junit.runner.JUnitCore $classes_teste
+    java -cp "$dir_epc_tool:$classpath_epc_tool" $2 $classes_teste
     java -jar $dir_epc_tool report -input ./coverage.ser -classes $dir_fonte -xml $dir_xml_epc_tool-edge-pair.xml -edge-pairs
     rm ./coverage.ser
 }
